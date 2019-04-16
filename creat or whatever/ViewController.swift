7b552b51698c645640e22475f3606f51b35b5fbe
james cotter <james.cotter@smail.astate.edu>
2019-04-15 20:39:05 -0500

    Initial Commit

---
+++ creat or whatever/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  creat or whatever
+//
+//  Created by James.Cotter on 4/15/19.
+//  Copyright © 2019 James Cotter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
